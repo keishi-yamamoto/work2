21ff738c0c41407cc8ff21c4d1525eabca4f28b8
vagrant <vagrant@localhost.localdomain>
2020-04-18 07:05:09 +0000

    [Finish] Rspec checked

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,21 +1,26 @@
 class BooksController < ApplicationController
+  before_action :ensure, {only: [:edit, :update]}
 
   def show
   	@book = Book.find(params[:id])
+    @user = User.find(@book.user_id)
+    @post_book = Book.new
   end
 
   def index
-  	@books = Book.all #一覧表示するためにBookモデルの情報を全てくださいのall
+  	@books = Book.all
+    @book = Book.new
   end
 
   def create
-  	@book = Book.new(book_params) #Bookモデルのテーブルを使用しているのでbookコントローラで保存する。
-  	if @book.save #入力されたデータをdbに保存する。
-  		redirect_to @book, notice: "successfully created book!"#保存された場合の移動先を指定。
-  	else
-  		@books = Book.all
-  		render 'index'
-  	end
+  	@book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      redirect_to book_path(@book), notice: "successfully created book!"
+    else
+      @books = Book.all
+      render 'index'
+    end
   end
 
   def edit
@@ -28,7 +33,7 @@ class BooksController < ApplicationController
   	@book = Book.find(params[:id])
   	if @book.update(book_params)
   		redirect_to @book, notice: "successfully updated book!"
-  	else #if文でエラー発生時と正常時のリンク先を枝分かれにしている。
+  	else
   		render "edit"
   	end
   end
@@ -38,11 +43,16 @@ class BooksController < ApplicationController
   	@book.destoy
   	redirect_to books_path, notice: "successfully delete book!"
   end
+  def ensure
+    if current_user.id != Book.find(params[:id]).user_id
+      redirect_to books_path
+    end
+  end
 
   private
 
   def book_params
-  	params.require(:book).permit(:title)
+  	params.require(:book).permit(:title, :body)
   end
 
 end
--- check.sh
+++ check.sh
@@ -1,5 +1,4 @@
-echo -e "\ngroup :test do\ngem 'rspec-rails', '~> 3.6.0'\ngem 'factory_bot_rails'\ngem 'rspec-parameterized'\ngem 'faker' #ランダム値の生成\ngem 'shoulda-matchers'\ngem 'rails-controller-testing'\nend" >> Gemfile
-cp -r ../../RspecBookers2Debug/spec/ ./
+cp -r ../../work/spec/ ./
 bundle install
 bundle exec rspec spec/ --format documentation 
 
